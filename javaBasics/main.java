43ff3271282938852d8b506a4211689ce99a10d7
Neel Patel <neel.patel@metropolia.fi>
2021-11-28 10:33:05 +0200

    - Added some exercises answers.

--- javaBasics/main.java
+++ javaBasics/main.java
@@ -215,6 +215,43 @@ class main {
         }
 
          */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Provide a value, a negative value ends the program");
+            int value = Integer.parseInt(scanner.nextLine());
+            if (value < 0) {
+                break;
+            }
+
+            values = values + 1;
+            sum = sum + value;
+        }
+
+        if (sum == 0) {
+            System.out.println("The average of the values could not be calculated.");
+        } else {
+            System.out.println("Average of values: " + (1.0 * sum / values));
+        }
+
+         */
+    /*
+        Scanner x = new Scanner(System.in);
+        System.out.print("Customer number: ");
+        int y = Integer.valueOf(x.nextLine());
+
+        if (y >= 1000 && y % 25 == 0) {
+            System.out.println("Gets a gift card!");
+        } else if (y % 2000 == 0) {
+            System.out.println("Gets a large gift card!");
+        } else {
+            System.out.println("Gets nothing.");
+        }
+        
+     */
 
     }
 }
