b2ca0bc522f5030c230ab22985f8facaf4ddb95a
Neel Patel <neel.patel@metropolia.fi>
2021-12-03 23:29:05 +0200

    - Adding a boject to a list of an array

--- javaBasics/main.java
+++ javaBasics/main.java
@@ -1,6 +1,51 @@
 import java.util.Arraylist;
 import java.util.Scanner;
 
+public class Person {
+
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+
+    public Person(String name) {
+        this.name = name;
+        this.age = 0;
+        this.weight = 0;
+        this.height = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void growOlder() {
+        this.age = this.age + 1;
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heightDivByHundred = this.height / 100.0;
+        return this.weight / (heightDivByHundred * heightDivByHundred);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
+    }
+}
+
 class Statistics
 {
     private int count;
@@ -748,6 +793,20 @@ class main {
 
 
  */
+        ArrayList<Person> persons = new ArrayList<>();
+
+// a person object can be created first
+        Person john = new Person("John");
+// and then added to the list
+        persons.add(john);
+
+// person objects can also be created "in the same sentence" that they are added to the list
+        persons.add(new Person("Matthew"));
+        persons.add(new Person("Martin"));
+
+        for (Person person: persons) {
+            System.out.println(person);
+        }
 
     }
 }
