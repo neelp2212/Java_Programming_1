e3527c1c7f1b1d8847d27cc50a90c3d15e9db2e3
Neel Patel <neel.patel@metropolia.fi>
2021-12-09 09:01:34 +0200

    - It ask user to give the text file name - It will print the numbers written it that file

---
+++ javaBasics/Number_From_a_file.java
@@ -0,0 +1,26 @@
+import java .util.Scanner;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class Number_From_a_file
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.parseInt(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.parseInt(scanner.nextLine());
+
+
+        int count = 0;
+
+        while ()
+
+    }
+}
+
